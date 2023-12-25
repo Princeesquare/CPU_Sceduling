ae07c2a18c38317415e2b1e8b8237719bf7eb32d
PRINCE EMMANUEL <prince19e@outlook.com>
2023-12-25 21:08:40 +0200

    Declaring the required linked list functions

--- Simulator.cpp
+++ Simulator.cpp
@@ -23,3 +23,13 @@ struct Algorithm
     int pModes; // Preemptive mode
     int n;     // time quantum for Round-Robin Scheduling
 } alg = { 4, 2, 2 };
+
+// App function declarations
+
+
+// Linked list function declarations
+struct Process* createProcess(int, int, int, int);
+struct Process* insertFront(Process*, int, int, int, int);
+struct Process* insertBack(Process*, int, int, int, int);
+void deleteProcesses(Process**);
+void sortProcesses(Process**, string);
