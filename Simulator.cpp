d838f0d981f6a6648e4efb1e4643ac5957beeeba
PRINCE EMMANUEL <prince19e@outlook.com>
2023-12-27 22:10:05 +0200

    Rewriting the code using queue functions because there's a lot of compilation error using linked list

--- Simulator.cpp
+++ Simulator.cpp
@@ -1,111 +1,34 @@
 #include <iostream>
 #include <fstream>
-#include <cstring>
-#include <cstdlib> 
+#include <vector>
+#include <algorithm>
 
-using namespace std;
+#define SIZE 5
+#define LINE_MAX 1024
 
-struct Process
-{
-    int pid;
-    int burstTime;
-    int arrivalTime;
-    int priority;
-    int wt;  // Process waiting time
-    int rbt; // Process remaining time
-    struct Process* next;
-    struct Process* prev;
+struct Process {
+    int Name;
+    int bTime; // Burst Time
+    int aTime; // Arrival Time
+    int wTime; // Waiting Time
+    int p;     // Priority
+    int sr;    // Show Result
 };
 
-struct Algorithm
-{
-    int method;
-    int pMode; // Preemptive mode
-    int n;     // time quantum for Round-Robin Scheduling
-} alg = { 0, 0, 0 };
-
-Process* initialHeader = nullptr;
-
-string methods[5] = { "None", "First Come, First Served Scheduling", "Shortest-Job-First Scheduling", "Priority Scheduling", "Round-Robin Scheduling" };
-string pModes[2] = { "OFF", "ON" };
-string outputFilename;
-
-// App function declarations
-void validateParams(int, char**);
-void getInitialProcesses(const string& filename);
-void loadMainMenu();
-void handleSchedulingMethod();
-void handlePMode();
-void handleShowResult();
-void handleEndProgram();
-
-// Linked list function declarations
-struct Process* createProcess(int, int, int, int);
-struct Process* insertFront(Process*, int, int, int, int);
-struct Process* insertBack(Process*, int, int, int, int);
-void deleteProcesses(Process**);
-void sortProcesses(Process**, string);
-
-int main(int argc, char** argv)
-{
-    validateParams(argc, argv);
-
-    outputFilename = argv[4];
-
-    getInitialProcesses(argv[2]);
-    loadMainMenu();
-
-    return 0;
+void enqueue(std::vector<Process>& queue, const Process& element) {
+    queue.push_back(element);
 }
 
-// App templates
-string mainMenuTemplate()
-{
-    string str = "CPU SCHEDULER SIMULATOR\n"
-        "1) Scheduling Method (" +
-        methods[alg.method] + (alg.method == 4 ? (" – time_quantum=" + to_string(alg.n)) : "") + ")\n"
-        "2) Preemptive Mode (" +
-        pModes[alg.pMode] + ")\n"
-        "3) Show Result\n"
-        "4) End Program\n\n";
-
-    return str;
+void dequeue(std::vector<Process>& queue) {
+    if (!queue.empty()) {
+        queue.erase(queue.begin());
+    }
 }
 
-string programResultTemplate(Process* header, Algorithm a = alg)
-{
-    float sum = 0;
-    int size = 0;
-    float avWaitingTime;
-    string mode = "";
+Process front(const std::vector<Process>& queue) {
+    return queue.front();
+}
 
-    // Sort processes to show up in order
-    sortProcesses(&header, "pid");
-
-    if (a.method == 4)
-    {
-        mode = " - time_quantum=" + to_string(a.n);
-    }
-    else if (a.method == 2 || a.method == 3)
-    {
-        mode = a.pMode ? " - Preemptive" : " - Non Preemptive";
-    }
-
-    string str = "Scheduling Method: " + methods[a.method] + mode + "\n"
-        "Process waiting times:\n";
-
-    while (header != nullptr)
-    {
-        str += "P" + to_string(header->pid) + ": " + to_string(header->wt) + " ms\n";
-        sum += header->wt;
-        size++;
-
-        header = header->next;
-    }
-
-    avWaitingTime = sum / size;
-
-    str += "Average Waiting Time: " + to_string(avWaitingTime) + " ms\n\n";
-
-    return str;
+bool isEmpty(const std::vector<Process>& queue) {
+    return queue.empty();
 }
