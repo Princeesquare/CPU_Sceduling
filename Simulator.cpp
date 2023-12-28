417961ea88b1d465c33a7248e4a4898a823eb3b8
PRINCE EMMANUEL <prince19e@outlook.com>
2023-12-28 21:27:22 +0200

    Round Robin Implementation

--- Simulator.cpp
+++ Simulator.cpp
@@ -32,3 +32,35 @@ Process front(const std::vector<Process>& queue) {
 bool isEmpty(const std::vector<Process>& queue) {
     return queue.empty();
 }
+
+void RoundRobin(std::vector<Process>& processes, int quantumTime) {
+    std::vector<Process> processQueue;
+    int currentTime = 0;
+
+    while (!isEmpty(processQueue) || currentTime < SIZE) {
+        while (currentTime < SIZE && processes[currentTime].aTime <= currentTime) {
+            enqueue(processQueue, processes[currentTime]);
+            currentTime++;
+        }
+
+        if (!isEmpty(processQueue)) {
+            Process currentProcess = front(processQueue);
+            dequeue(processQueue);
+
+            int executionTime = std::min(quantumTime, currentProcess.bTime);
+            currentProcess.bTime -= executionTime;
+
+            for (int i = currentTime; i < currentTime + executionTime; i++) {
+                processes[i].wTime += executionTime;
+            }
+
+            currentTime += executionTime;
+
+            if (currentProcess.bTime > 0) {
+                enqueue(processQueue, currentProcess);
+            }
+        }
+        else {
+            currentTime++;
+        }
+    }
